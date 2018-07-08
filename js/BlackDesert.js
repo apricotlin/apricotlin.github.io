5a8b9e26481cac1ae961e923bb0c649226672247
apricotlin <apricotlin@gmail.com>
2018-07-08 19:53:51 +0800

    20180708
    新增BUG說明用html，修改一些檔名，刪掉測試html

--- js/BlackDesert.js
+++ js/BlackDesert.js
@@ -1,37 +1,37 @@
 // NPC材料價格
 var npcItemPrice = {
 		"salt": {
-			"price": 20
+			"price": 20 // 鹽
 		},
 		"suger": {
-			"price": 20
+			"price": 20 // 糖
 		},
 		"leaven": {
-			"price": 20
+			"price": 20 // 發酵劑
 		},
 		"oil": {
-			"price": 20
+			"price": 20 // 沙拉油
 		},
 		"seaSalt": {
-			"price": 200
+			"price": 200 // 海鹽
 		},
 		"blackSuger": {
-			"price": 200
+			"price": 200 // 黑糖
 		},
 		"oliveOil": {
-			"price": 40
+			"price": 40 // 橄欖油
 		},
 		"friedOil": {
-			"price": 40
+			"price": 40 // 油炸用油
 		},
 		"wine": {
-			"price": 40
+			"price": 40 // 料理用紅酒
 		},
 		"seasoning": {
-			"price": 40
+			"price": 40 // 基本醬料
 		},
 		"water": {
-			"price": 30
+			"price": 30 // 料理用飲用水
 		},
 	};
 
